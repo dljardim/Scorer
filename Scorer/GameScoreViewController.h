40912426dc10741a6be8e113cfb8327801e97951
Damian Jardim <dljardim@outlookcom>
2014-09-20 04:24:18 -0700

    added gamescorecontroller

---
+++ Scorer/GameScoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameScoreViewController.h
+//  Scorer
+//
+//  Created by Damian Jardim on 9/20/14.
+//  Copyright (c) 2014 Damian Jardim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameScoreViewController : UIViewController
+
+@end
